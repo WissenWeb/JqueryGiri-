24fa7b2f00118c9c9600deb651069faab4e61b55
BRIGHT <wissen30@hotmail.com>
2023-11-04 14:21:17 +0300

    Add project files.

---
+++ JqueryGiriş/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JqueryGiriş.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
